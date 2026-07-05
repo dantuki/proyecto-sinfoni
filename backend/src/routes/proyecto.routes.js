e2fdcc1cc3b3000230d63e97e5c0ae2cc5478829
Dante <danielbedoyalopez5@gmail.com>
2026-07-05 15:28:30 -0500

    error en las rutas solucionado

--- backend/src/routes/proyecto.routes.js
+++ backend/src/routes/proyecto.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { obtenerMisProyectos, obtenerMisParticipaciones } = require('../controllers/proyecto.controller');
-const verificarToken = require('../middlewares/auth.middleware'); // Tu middleware de protección
+const verificarToken = require('../middleware/authMiddleware');
 
-// Ambas rutas protegidas por token para asegurar que nadie husmee proyectos ajenos
+// Ambas rutas protegidas por token
 router.get('/director/:id', verificarToken, obtenerMisProyectos);
 router.get('/participante/:id', verificarToken, obtenerMisParticipaciones);
 
