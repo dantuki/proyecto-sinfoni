c1fc9c9dded9894079303214d210f937b0bbe466
Dante <danielbedoyalopez5@gmail.com>
2026-07-08 10:34:08 -0500

    mejoramos las rutas de las solicitudes

--- backend/src/routes/solicitudRoutes.js
+++ backend/src/routes/solicitudRoutes.js
@@ -8,14 +8,9 @@ const upload = require('../middleware/uploadMiddleware');
 router.get('/', solicitudController.getSolicitudes);
 router.get('/:id', solicitudController.getSolicitudById);
 
-// Rutas Protegidas y con Subida de Archivos
-// Se valida el token y luego se interceptan los PDFs si los hay
-router.post('/', verificarToken, upload.fields([
-{ name: 'doc_par_1', maxCount: 1 }, 
-{ name: 'doc_par_2', maxCount: 1 }
-]), solicitudController.createSolicitud);
-
-router.put('/:id', verificarToken, solicitudController.updateSolicitud);
+// Rutas Protegidas con Subida Dinámica de Archivos en Arreglo
+router.post('/', verificarToken, upload.array('archivos', 10), solicitudController.createSolicitud);
+router.put('/:id', verificarToken, upload.array('archivos', 10), solicitudController.updateSolicitud);
 router.delete('/:id', verificarToken, solicitudController.deleteSolicitud);
 
 module.exports = router;
