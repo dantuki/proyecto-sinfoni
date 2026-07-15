2ea251d889655a1d006b8d07daedc430cc1ae7e1
Dante <danielbedoyalopez5@gmail.com>
2026-07-15 16:51:14 -0500

    rutas listas

--- backend/src/routes/sedeRoutes.js
+++ backend/src/routes/sedeRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const sedeController = require('../controllers/sedeController');
+const SedeController = require('../controllers/sedeController');
 
-router.get('/', sedeController.getSedes);          // GET general (Listar)
-router.get('/:id', sedeController.getSedeById);     // GET específico (Info)
-router.post('/', sedeController.createSede);        // POST (Agregar)
-router.put('/:id', sedeController.updateSede);      // PUT (Update)
-router.delete('/:id', sedeController.deleteSede);   // DELETE (Borrar)
+router.get('/', SedeController.getSedes);          // GET general (Listar)
+router.get('/:id', SedeController.getSedeById);     // GET específico (Info)
+router.post('/', SedeController.createSede);        // POST (Agregar)
+router.put('/:id', SedeController.updateSede);      // PUT (Update)
+router.delete('/:id', SedeController.deleteSede);   // DELETE (Borrar)
 
 module.exports = router;
